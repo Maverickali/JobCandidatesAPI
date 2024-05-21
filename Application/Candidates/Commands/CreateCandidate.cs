48f3994598408c168063e0e9da1c41430a7b3d53
Collins Ddumba <collins@databalk.nu>
2024-05-21 14:41:47 +0200

    updating readme file

--- Application/Candidates/Commands/CreateCandidate.cs
+++ Application/Candidates/Commands/CreateCandidate.cs
@@ -3,52 +3,63 @@ using Domain.Entities;
 using Infrastructure.Data;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Candidates.Commands
 {
     public class CreateCandidate : IRequestHandler<CreateCandidateCommandDto, int>
     {
         private readonly JobCandidatesContext _context;
+        private readonly Logger<JobCandidatesContext> _logger;
 
-        public CreateCandidate(JobCandidatesContext context)
+        public CreateCandidate(JobCandidatesContext context, Logger<JobCandidatesContext> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<int> Handle(CreateCandidateCommandDto request, CancellationToken cancellationToken)
         {
-            var existingCandidate = await _context.Candidates.FirstOrDefaultAsync(x => x.Email == request.Email);
-
-            if (existingCandidate != null)
+            try
             {
-                existingCandidate.FirstName = request.FirstName;
-                existingCandidate.LastName = request.LastName;
-                existingCandidate.PhoneNumber = request.PhoneNumber;
-                existingCandidate.PreferredCallTime = request.PreferredCallTime;
-                existingCandidate.FreeTextComment = request.FreeTextComment;
-                existingCandidate.LinkedInProfile = request.LinkedInProfile;
-                existingCandidate.GitHubProfile = request.GitHubProfile;
-                existingCandidate.LastModified = DateTime.Now;
+                var existingCandidate = await _context.Candidates.FirstOrDefaultAsync(x => x.Email == request.Email);
+
+                if (existingCandidate != null)
+                {
+                    existingCandidate.FirstName = request.FirstName;
+                    existingCandidate.LastName = request.LastName;
+                    existingCandidate.PhoneNumber = request.PhoneNumber;
+                    existingCandidate.PreferredCallTime = request.PreferredCallTime;
+                    existingCandidate.FreeTextComment = request.FreeTextComment;
+                    existingCandidate.LinkedInProfile = request.LinkedInProfile;
+                    existingCandidate.GitHubProfile = request.GitHubProfile;
+                    existingCandidate.LastModified = DateTime.Now;
+                    await _context.SaveChangesAsync(cancellationToken);
+                    return existingCandidate.Id;
+                }
+                var entity = new Candidate
+                {
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    Email = request.Email,
+                    PhoneNumber = request.PhoneNumber,
+                    PreferredCallTime = request.PreferredCallTime,
+                    LinkedInProfile = request.LinkedInProfile,
+                    GitHubProfile = request.GitHubProfile,
+                    FreeTextComment = request.FreeTextComment,
+                    Created = DateTime.Now
+                };
+
+                _context.Add<Candidate>(entity);
                 await _context.SaveChangesAsync(cancellationToken);
-                return existingCandidate.Id;
+
+                return entity.Id;
             }
-            var entity = new Candidate
+            catch (Exception ex)
             {
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                Email = request.Email,
-                PhoneNumber = request.PhoneNumber,
-                PreferredCallTime = request.PreferredCallTime,
-                LinkedInProfile = request.LinkedInProfile,
-                GitHubProfile = request.GitHubProfile,
-                FreeTextComment = request.FreeTextComment,
-                Created = DateTime.Now
-            };
-
-            _context.Add<Candidate>(entity);
-            await _context.SaveChangesAsync(cancellationToken);
-
-            return entity.Id;
+                _logger.LogInformation($"Error Message - {ex.Message}");
+            }
+            return 0;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # JobCandidatesAPI
 
+## List of Ways for Improvement
+
+### 1. Adding Authentication and Authorization
+- Implement API key or OAuth2 for securing the endpoint.
+- Ensure only authorized applications can access and modify candidate information.
+
+### 2. Adding Logging and Monitoring Tools
+- Integrate tools like Application Insights (AppInsights) to log API requests.
+- Set up monitoring to track performance metrics and uptime.
+- Implement detailed logging for easier debugging and auditing.
+
+### 3. Adding API Versioning
+- Implement versioning to manage changes and updates in the API.
+- Ensure backward compatibility for existing clients while allowing for enhancements and new features.
+
+### 4. Considering Caching and Load Balancing
+- Use caching strategies to reduce the load on the database and improve response times for frequently accessed data.
+- Design the application to support load balancing to handle high volumes of requests efficiently.
