7eb0a26061ffa0205fb02bc93b3ab4a9313fe448
Collins Ddumba <collins@databalk.nu>
2024-05-21 11:03:32 +0200

    setuping Clean architecture

---
+++ Infrastructure/Data/JobCandidatesContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class JobCandidatesContext : DbContext
+    {
+        public JobCandidatesContext(DbContextOptions<JobCandidatesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
